6352cca38e67bc0f599c48743be6c9a76e618279
Sam Gammon <sam@momentum.io>
2017-04-26 18:34:11 -0700

    Further work on Python build chain

---
+++ Python/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+  bloombox: setup
+  ~~~~~~~~~~~~~~~
+
+  Egg builder script for the Bloombox API Client for Python.
+
+  :author: Sam Gammon <sg@samgammon.com>
+  :copyright: (c) Sam Gammon, 2017
+  :license: This software makes use of the Apache 2.0 License.
+            A copy of this license is included as ``LICENSE.txt`` in
+            the root of the project.
+"""
